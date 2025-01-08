8a0530c5f718fec6ed1802a016ff21ba5f715818
Izaias Castro <30877119+IzaiasCastro@users.noreply.github.com>
2025-01-08 17:41:19 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import psycopg2
 import os
 
 # Exemplo de conexão com o banco de dados
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("SUPABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecretkey")  # Define um valor padrão para evitar erros
 
 
