d1be14bbe71f1035a5375ad15ceb6b005045c2ee
Izaias Castro <30877119+IzaiasCastro@users.noreply.github.com>
2025-01-09 08:29:21 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import psycopg2
 import os
 
 # Exemplo de conexão com o banco de dados
-DATABASE_URL = os.getenv("POSTGRES_PRISMA_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecretkey")  # Define um valor padrão para evitar erros
 
 
